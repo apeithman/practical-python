25c4cee3f99335a30d2028b514aa00c845572b92
apeithman <adampeithman@gmail.com>
2024-02-05 00:23:53 +0700

    finished basic function

--- Work/bounce.py
+++ Work/bounce.py
@@ -1,3 +1,10 @@
 # bounce.py
 #
 # Exercise 1.5
+height = 100
+bounce_factor = 3/5
+bounce_number = 1
+while bounce_number < 11:
+    height = bounce_factor * height
+    print(bounce_number, round(height, 4))
+    bounce_number += 1
